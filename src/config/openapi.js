d430524227a015ab523e60d448dac09b0f8638f5
crosschainer <68580992+crosschainer@users.noreply.github.com>
2025-05-31 17:56:13 +0200

    Update openapi.js

--- src/config/openapi.js
+++ src/config/openapi.js
@@ -197,7 +197,7 @@ export const openapiSpec = {
                     name: { type: "string", example: "con_usdc" },
                     token_name: { type: "string", example: "USDC" },
                     token_symbol: { type: "string", example: "USDC" },
-                    supply: { type: "string", example: "1000000.00000000" },
+                    display: { type: "string", example: "USDC (USDC)" },
                     created_at: { type: "string", format: "date-time", example: "2023-01-01T00:00:00Z" }
                   }
                 }
