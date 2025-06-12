7af0723f1ed99bec527cb1c4d3e2203ae7e4f449
crosschainer <eodkwokm@gmail.com>
2025-06-12 10:57:05 +0000

    things

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@
  */
 
 import router            from "./routes/router.js";
+import { StatsObject }   from "./objects/statsObject.js";
+
+/* ---- expose DO class to Wrangler ---- */
+export { StatsObject };
 
 export default {
   async fetch(request, env, ctx) {
