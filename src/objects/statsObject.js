5b940c304ff90f4ad5bd02dd3134eb73ef260fb6
crosschainer <eodkwokm@gmail.com>
2025-06-13 05:50:36 +0000

    can

--- src/objects/statsObject.js
+++ src/objects/statsObject.js
@@ -64,7 +64,7 @@ export class StatsObject {
     this.ready = this._loadSnapshot().catch(() => {});
 
     // first alarm ASAP
-    this.state.storage.setAlarm(Date.now());
+    //this.state.storage.setAlarm(Date.now());
   }
 
   /*──────────────── snapshot helpers ────────────────*/
